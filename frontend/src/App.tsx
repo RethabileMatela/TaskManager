3baef534bba8f2f06392275537e8e6adcc855552
RethabileMatela <matelarethabile@gmail.com>
2025-05-24 19:09:00 +0200

    navbar ui created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
+import { BrowserRouter } from 'react-router-dom';
 import './App.css'
+import Home from './pages/Home';
 
 function App() {
-
   return (
-    <h1 className="text-3xl font-bold bg-pink-300">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar"
+
+const Home = () => {
+  return (
+    <div className="w-full h-screen flex flex-col items-center justify-center">
+        <Navbar/>
+        home
+    </div>
+  )
+}
+
+export default Home
