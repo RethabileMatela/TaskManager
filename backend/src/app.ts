a21a6098dbec43c4b1496d3c73b9127b8a316b6e
RethabileMatela <matelarethabile@gmail.com>
2025-05-25 18:55:04 +0200

    run server

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,15 @@
 import express from 'express';
 import { errorHandler } from './middlewares/errorHandler';
-import userRoutes from './routes/userRoutes';
-import { helloWorld } from './controllers/userController';
+import userRoutes from './user/routes/userRoutes';
 
 const app = express();
 
 app.use(express.json());
 
 // Routes
-app.use('/api/users', userRoutes);
+app.use('/api/items', userRoutes);
+
+// Global error handler (should be after routes)
+app.use(errorHandler);
 
 export default app;
