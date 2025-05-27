c34e4221ef51ddd387e1c855d4b995d57d3819d7
RethabileMatela <matelarethabile@gmail.com>
2025-05-27 19:07:05 +0200

    font src

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -19,8 +19,9 @@ app.use('/api/users', userRoutes);
 
 app.use(function (req, res, next) {
    res.setHeader(
-     'Content-Security-Policy-Report-Only', "default-src 'none';"
+     'Content-Security-Policy', "default-src 'self'; script-src 'self'; style-src 'self'; font-src 'self'; img-src 'self'; frame-src 'self'"
    );
+   
    next();
  });
 
