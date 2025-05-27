ead61059723a4a79ff179b287a54cfb27c84fe45
RethabileMatela <matelarethabile@gmail.com>
2025-05-27 15:54:00 +0200

    use strict

--- backend/dist/user/routes/userRoutes.js
+++ backend/dist/user/routes/userRoutes.js
@@ -1,4 +1,3 @@
-"use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const userController_1 = require("../controllers/userController");
