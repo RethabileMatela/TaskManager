928b49d02858db26aad7fb39b8409cfb84f0aca2
RethabileMatela <matelarethabile@gmail.com>
2025-05-27 20:09:15 +0200

    cps

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -16,10 +16,11 @@ app.use(express.json());
 
 app.use(cors());
 
-app.use(function(req, res, next) {
-   res.setHeader("Content-Security-Policy", "default-src 'self'; font-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'self';");
-   return next();
+app.use(function (req: Request, res: Response, next: NextFunction) {
+   res.setHeader("content-security-policy-report-only", "default-src 'self'; script-src 'self' 'report-sample'; style-src 'self' 'report-sample'; base-uri 'none'; object-src 'none'; report-uri https://5e52f4c893efcda6a7d40460.endpoint.csper.io");
+   next();
 });
+ 
 app.use('/api/users', userRoutes);
 
 // Global error handler (should be after routes)
