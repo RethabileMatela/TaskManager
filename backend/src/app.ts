f83a91e0f4189329b05662682c0b56b5c130f40f
RethabileMatela <matelarethabile@gmail.com>
2025-05-27 19:26:15 +0200

    fix cps

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,6 +6,7 @@ import { errorHandler } from './middlewares/errorHandler';
 import userRoutes from './user/routes/userRoutes';
 // @ts-ignore
 import cors from 'cors';
+import { expressCspHeader, SELF, INLINE, NONE } from 'express-csp-header';
 
 import { Request, Response, NextFunction } from 'express';
 
@@ -16,13 +17,16 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/api/users', userRoutes);
-
-app.use(function (req, res, next) {
-   res.setHeader(
-     'Content-Security-Policy-Report-Only', "default-src 'self'; script-src 'self' https://code.jquery.com https://cdnjs.cloudflare.com https://stackpath.bootstrapcdn.com https://cdn.jsdelivr.net 'sha256-ISInfOBSUWFgeTRFLf63g+rFNSswGDl15oK0iXgYM='; style-src 'self' https://stackpath.bootstrapcdn.com https://cdnjs.cloudflare.com; font-src 'self' https://cdnjs.cloudflare.com; img-src 'self'; frame-src 'self'"
-   );
-   next();
- });
+app.use(expressCspHeader({
+   directives: {
+       'default-src': [SELF],
+       'script-src': [SELF, INLINE, 'somehost.com'],
+       'style-src': [SELF, 'mystyles.net'],
+       'img-src': ['data:', 'images.com'],
+       'worker-src': [NONE],
+       'block-all-mixed-content': true
+   }
+}));
 
 // Global error handler (should be after routes)
 app.use(errorHandler);
