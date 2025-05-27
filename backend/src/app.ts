ccc0f4a72738780909ba1955bf35d83c2c5e4b59
RethabileMatela <matelarethabile@gmail.com>
2025-05-27 19:38:35 +0200

    CPS

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -16,17 +16,17 @@ app.use(express.json());
 
 app.use(cors());
 
-app.use('/api/users', userRoutes);
 app.use(expressCspHeader({
    directives: {
-       'default-src': [SELF],
-       'script-src': [SELF, INLINE, 'somehost.com'],
-       'style-src': [SELF, 'mystyles.net'],
+       'default-src': [NONE],
+       'script-src': [NONE, INLINE, 'somehost.com'],
+       'style-src': [NONE, 'mystyles.net'],
        'img-src': ['data:', 'images.com'],
        'worker-src': [NONE],
        'block-all-mixed-content': true
    }
 }));
+app.use('/api/users', userRoutes);
 
 // Global error handler (should be after routes)
 app.use(errorHandler);
