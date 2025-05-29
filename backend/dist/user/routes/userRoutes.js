47724ea8720838fe246afcdddc783120d53175ee
RethabileMatela <matelarethabile@gmail.com>
2025-05-29 16:58:04 +0200

    build

---
+++ backend/dist/user/routes/userRoutes.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// @ts-ignore
+const express_1 = require("express");
+const userController_1 = require("../controllers/userController");
+const router = (0, express_1.Router)();
+router.get('/', userController_1.getAllUsers);
+router.get('/:id', userController_1.getUserById);
+router.post('/', userController_1.createUser);
+router.put('/:id', userController_1.updateUser);
+router.delete('/:id', userController_1.deleteUser);
+router.post('/:id/tasks', userController_1.createTask);
+router.get('/:id/tasks', userController_1.getAllTasksByUserId);
+exports.default = router;
